93e7fc774356e759012c8ebc9ba246601bac6cfe
Shamroza Falak <malikshamroza2@gmail.com>
2026-06-25 02:56:39 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:3000",
+      "https://portfoliowebsitebackend-0zs7.onrender.com",
     ],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
